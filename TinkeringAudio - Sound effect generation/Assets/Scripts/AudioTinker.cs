09319cd3caa873c7fdf724c68b2dd1798d1f82fc
EthanJones2001 <56162439+EthanJones2001@users.noreply.github.com>
2019-11-19 17:31:30 +0000

    Re-Added project

---
+++ TinkeringAudio - Sound effect generation/Assets/Scripts/AudioTinker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class AudioTinker : MonoBehaviour
+{
+    public int frequency;
+    private AudioSource audioSource;
+    private AudioClip outAudioClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        outAudioClip = CreateToneAudioClip(frequency);
+    }
+
+    public void PlayOutAudio()
+    {
+        audioSource.PlayOneShot(outAudioClip);
+    }
+
+
+    public void StopAudio()
+    {
+        audioSource.Stop();
+    }
+
+
+    private AudioClip CreateToneAudioClip(int frequency)
+    {
+        int sampleDurationSecs = 3;
+        int sampleRate = 70000;
+        int sampleLength = sampleRate * sampleDurationSecs;
+        float maxValue = 3f / 10f;
+
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++)
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+
+
+}
