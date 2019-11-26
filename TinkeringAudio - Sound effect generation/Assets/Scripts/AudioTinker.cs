e222d73a7b59c3d6775b087e447e2429cacb67ae
EthanJones2001 <56162439+EthanJones2001@users.noreply.github.com>
2019-11-26 10:06:18 +0000

    Improving duration of sounds

--- TinkeringAudio - Sound effect generation/Assets/Scripts/AudioTinker.cs
+++ TinkeringAudio - Sound effect generation/Assets/Scripts/AudioTinker.cs
@@ -34,7 +34,6 @@ public class AudioTinker : MonoBehaviour
         audioSource.Stop();
     }
 
-
     public AudioClip CreateToneAudioClip(int frequency, float sampleDur)
     {
         float sampleDurationSecs = sampleDur;
